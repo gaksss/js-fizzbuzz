432c176f2e59ffc506e211d0847422275ba689f5
gaksss <clement.carlier2@gmail.com>
2024-11-12 12:11:11 +0100

    deuxieme solution

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,31 @@
-let num = 4;
+let num = 45;
+
+// if (num % 3 === 0 && num % 5 != 0){
+//     console.log("fizz")
+// }
+
+// if (num % 5 === 0 && num % 3 != 0){
+//     console.log("buzz")
+// }
+
+// if (num % 5 === 0 && num % 3 === 0){
+//     console.log("fizzbuzz")
+// }
+
+// if (num % 5 !== 0 && num % 3 != 0){
+//     console.log(num)
+// }
 
 if (num % 3 === 0 && num % 5 != 0){
     console.log("fizz")
 }
-
-if (num % 5 === 0 && num % 3 != 0){
+else  if(num % 5 === 0 && num % 3 != 0){
     console.log("buzz")
 }
 
-if (num % 5 === 0 && num % 3 === 0){
-    console.log("fizzbuzz")
-}
-
-if (num % 5 !== 0 && num % 3 != 0){
-    console.log(num)
-}
+else if (num % 5 === 0 && num % 3 === 0){
+     console.log("fizzbuzz")
+    }
+    else {
+        console.log(num)
+    }
